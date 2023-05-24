62572ab8de087025af3f191b1b0da397ddb739e4
Valeri <valeri.torishnya@gmail.com>
2023-05-24 22:40:17 +0300

    Update files

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).json('OK'));
+router.get('/', (req, res) => res.status(200).json('OKAY'));
 router.get('/test', (req, res) => res.status(200).json('TEST-OK'));
 
 export default router;
